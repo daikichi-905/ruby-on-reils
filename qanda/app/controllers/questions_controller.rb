6c225cfe188f1d36ecd05706390c8a24298d4f96
y.daikichi <yamadai.cell@gmail.com>
2023-11-19 10:16:31 +0000

    11/19の更新

--- qanda/app/controllers/questions_controller.rb
+++ qanda/app/controllers/questions_controller.rb
@@ -22,21 +22,34 @@ class QuestionsController < ApplicationController
     # Questionモデルを初期化
     @question = Question.new(questions_params)
     # QuestionモデルをDBへ保存
-    @question.save
-    # showへリダイレクト
-    redirect_to @question
+    if @question.save
+      # showへリダイレクト
+      redirect_to @question
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
   
   # 質問の編集
   def edit
+    @question = Question.find(params[:id])
   end
 
   # 質問の更新
   def update
+    @question = Question.find(params[:id])
+    if @question.update(questions_params)
+      redirect_to @question
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   # 質問の削除
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
   
   private
--- qanda/config/routes.rb
+++ qanda/config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :questions
+  # root "articles#index"
+  root "questions#index"
+  resources :questions do
+    resources :answers
+  end
 end
